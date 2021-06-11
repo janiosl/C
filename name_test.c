a7cd2d3bb5cfb1602ff6a4dfc8123c2e004f8bbd
Janio de Souza Lima <56620033+janiosl@users.noreply.github.com>
2021-06-11 09:56:09 -0300

    Update repo

--- name_test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    char first_name[20];
-    char last_name[20];
-    printf("Enter first name and last name: ");
-    scanf("%19s %19s", first_name, last_name);
-    printf("First name: %s Last name: %s\n", first_name, last_name);
-    return 0;
-}
